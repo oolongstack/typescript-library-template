5156ccd3c773bca3673f0e36cd9eb4e7ff0fbf21
dduohereshui <2245675739@qq.com>
2022-09-24 22:44:01 +0800

    first commit

---
+++ rollup.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+export default defineConfig([
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "dist/bundle.cjs",
+        format: "umd",
+        name: "ToolBundle",
+      },
+      {
+        file: "dist/bundle.mjs",
+        format: "esm",
+      },
+    ],
+    plugins: [typescript({ compilerOptions: { lib: ["esnext"] } })],
+    watch: {
+      exclude: "node_modules/**",
+    },
+  },
+  // 生成声明文件
+  {
+    input: "./src/index.ts",
+    output: [{ file: "dist/bundle.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+]);
